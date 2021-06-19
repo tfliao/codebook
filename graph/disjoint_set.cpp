c5314845b48229f3b0451a0b81a89b34b16c4c7a
Ting-Fu Liao <tingfuliao@gmail.com>
2021-06-20 01:42:46 +0800

    Add merge_to in disjoint set, with some comments

--- graph/disjoint_set.cpp
+++ graph/disjoint_set.cpp
@@ -6,13 +6,22 @@ public:
     DisjointSet(int N = 1) {
         _sz.assign(N, -1);
     }
+    /*
+        find root node of subtree contains x
+    */
     int find(int x) {
         if (_sz[x] < 0) return x;
         else return _sz[x] = this->find(_sz[x]);
     }
+    /*
+        get size of subtree contains x
+    */
     int size(int x) {
         return -_sz[find(x)];
     }
+    /*
+        merge two subtrees, use the one with more children as root
+    */
     bool merge(int x, int y) {
         int rx = this->find(x), ry = this->find(y);
         if (rx != ry) {
@@ -20,6 +29,18 @@ public:
                 swap(rx, ry);
             _sz[rx] += _sz[ry];
             _sz[ry] = rx;
+            return true;
+        }
+        return false;
+    }
+    /*
+        merge subtree c to subtree p, ingore size comparasion
+    */
+    bool merge_to(int c, int p) {
+        int rc = this->find(c), rp = this->find(p);
+        if (rx != ry) {
+            _sz[rp] += _sz[rc];
+            _sz[rc] = rp;
             return true;
         }
         return false;
